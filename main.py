ad6271f07fe673c193257746cefed2d19fc57fd8
Sergio Candalija Valderrama <sergiocava.d@gmail.com>
2020-05-20 17:30:20 +0200

    funciona para uno, ara sí

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 from ibm_botocore.exceptions import ClientError
 
 
-BUCKET_NAME = 'javiermartinezbucket1'
+BUCKET_NAME = 'deposit-sd-2020'
 resultFile = 'result.txt'
 askPermissionFile = 'p_write_'
 grantPermissionFile = 'write_'
@@ -18,49 +18,60 @@ key = 'bc586c5b02fc0842160010ce6bfb54b529fd7e82003ecd0b'
 N_SLAVES = 1 # TEST ONLY: Tiene que pedirse por teclado
 
 def master(id, x, ibm_cos):
-    empty = []
-    res = pickle.dumps(empty, protocol=0)
-    ibm_cos.put_object(Bucket=BUCKET_NAME, Key=resultFile, Body=res)
+    # askPermissionFileB = 'p_write_1'
+
+    time.sleep(20)
+    # ibm_cos.put_object(Bucket=BUCKET_NAME, Key=askPermissionFileB, Body='')
+    # empty = []
+    # res = pickle.dumps(empty, protocol=0)
+    ibm_cos.put_object(Bucket=BUCKET_NAME, Key=resultFile, Body=b'0')
+    resultTime = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=resultFile)['Contents'][0]['LastModified']
      # 1. monitor COS bucket each X seconds
     data = []
-    files = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=askPermissionFile)['Contents']
-    while len(files) > 0:
+    try:
+        files = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=askPermissionFile)['Contents']
         # 2. List all "p_write_{id}" files
         # dictionario de los archivos: data.append(a['Contents'])
         bucket_content = []
         for elem in files:
             bucket_content.append({"Key": elem['Key'], "LastModified":  elem['LastModified']})
+        data.append('2')
 
         # 3. Order objects by time of creation
-        bucket_content = sorted(bucket_content, key=lambda k: k['LastModified']) 
+        bucket_content = sorted(bucket_content, key=lambda k: k['LastModified'])
+        data.append('3')
 
         # 4. Pop first object of the list "p_write_{id}" 
         askedPermission = bucket_content.pop()['Key']
+        data.append('4')
 
         # 5. Write empty "write_{id}" object into COS
         grantPermission = askedPermission.replace(askPermissionFile, '')
         grantPermission = grantPermissionFile + str(grantPermission)
         data.append(grantPermission)
         ibm_cos.put_object(Bucket=BUCKET_NAME, Key=grantPermission)
+        data.append('5')
 
         # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
         ibm_cos.delete_object(Bucket=BUCKET_NAME, Key=askedPermission)
+        data.append('6')
 
         # 7. Monitor "result.json" object each X seconds until it is updated  
-        hasChanged = True
+        hasChanged = False
         while not hasChanged:
-            try:
-                filesToMonitor = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=askPermissionFile)['Contents']
-                hasChanged = files.items == filesToMonitor.items()
-                time.sleep(x)
-            except KeyError:
-                hasChanged = True   
+            newResultTime = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=resultFile)['Contents'][0]['LastModified']
+            hasChanged = newResultTime == resultTime
+            if hasChanged: resultTime = newResultTime
+            else: time.sleep(x)
         # 8. Delete from COS “write_{id}”
         ibm_cos.delete_object(Bucket=BUCKET_NAME, Key=grantPermission)
         # 9. Back to step 1 until no "p_write_{id}" objects in the bucket
         # files = ibm_cos.list_objects(Bucket=BUCKET_NAME, Prefix=askPermissionFile)['Contents']
         time.sleep(x)
-    return data
+        return data
+    except Exception as e:
+        data.append(e)
+        return data
 
 def slave(id, x, ibm_cos): 
     name = askPermissionFile + str(id)
@@ -75,11 +86,8 @@ def slave(id, x, ibm_cos):
         except ClientError as ex:
             if ex.response['Error']['Code'] == 'NoSuchKey':
                 time.sleep(x)
-    resultD = ibm_cos.get_object(Bucket=BUCKET_NAME, Key=resultFile)
-    resultData = pickle.loads(resultD)
-    resultData.append(id)
-    serialized = pickle.dumps(resultData, protocol=0) # protocol 0 is printable ASCII
-    ibm_cos.put_object(Bucket=BUCKET_NAME, key=resultFile, Body=serialized)
+    resultData = ibm_cos.get_object(Bucket=BUCKET_NAME, Key=resultFile)
+    ibm_cos.put_object(Bucket=BUCKET_NAME, Key=resultFile, Body=b'Hola')
     # No need to return anything """
 
 if __name__ == '__main__':
